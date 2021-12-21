ef70aa0eed784921306a2141d31d681cdd88a2da
kyomin <kim031504@naver.com>
2021-12-22 03:34:47 +0900

    옵션 정보 가져오기 및 테스트

--- src/pages/OrderPage/Options.js
+++ src/pages/OrderPage/Options.js
@@ -1,7 +1,12 @@
 import React from 'react';
 
-function Options() {
-	return <div></div>;
+function Options({ name }) {
+	return (
+		<form>
+			<input type='checkbox' id={`${name} option`} />{' '}
+			<label htmlFor={`${name} option`}>{name}</label>
+		</form>
+	);
 }
 
 export default Options;
